29caf7b75c3739d0aebd7330d3574c37f160f40a
Mahendra022 <ekamahendra86@gmail.com>
2021-06-15 21:54:33 +0700

    Project2_Creating World Time App

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,6 @@
 import 'package:flutter/material.dart';
-import 'quote.dart';
-import 'quote_card.dart';
+import 'pages/home.dart';
 
 void main() => runApp(MaterialApp(
-      home: QuoteList(),
+      home: Home(),
     ));
-
-class QuoteList extends StatefulWidget {
-  @override
-  _QuoteListState createState() => _QuoteListState();
-}
-
-class _QuoteListState extends State<QuoteList> {
-  List<Quote> quotes = [
-    Quote(author: 'Mahendra', text: 'When you try learn english?'),
-    Quote(author: 'Mahendra', text: 'What do you think about english?'),
-    Quote(author: 'Mahendra', text: 'Tell you experiance to learn english?'),
-  ];
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.grey[200],
-      appBar: AppBar(
-        title: Text('Awesome Quotes'),
-        centerTitle: true,
-        backgroundColor: Colors.redAccent,
-      ),
-      body: Column(
-        children: quotes
-            .map((quote) => QuoteCard(
-                quote: quote,
-                delete: () {
-                  setState(() {
-                    quotes.remove(quote);
-                  });
-                }))
-            .toList(),
-      ),
-    );
-  }
-}
