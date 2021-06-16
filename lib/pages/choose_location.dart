c8eb07becb18fef2cb9c7768944a85fca612914a
Mahendra022 <ekamahendra86@gmail.com>
2021-06-16 20:25:22 +0700

    Learn16_Project2-Asynchronous Code

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -6,34 +6,37 @@ class ChooseLocation extends StatefulWidget {
 }
 
 class _ChooseLocationState extends State<ChooseLocation> {
-  int counter = 0;
+  void getData() async {
+    //simulate network request for a username
+    String username = await Future.delayed(Duration(seconds: 3), () {
+      return 'Mahendra';
+    });
+
+    //simulate network request to get bio of the username
+    String bio = await Future.delayed(Duration(seconds: 2), () {
+      return 'Vega, musician & egg collector';
+    });
+    print('$username - $bio');
+  }
 
   @override
   void initState() {
     super.initState();
-    print('initState function ran');
+    getData();
+    print('Test 1');
   }
 
   @override
   Widget build(BuildContext context) {
-    print('build function ran');
     return Scaffold(
-      backgroundColor: Colors.grey[200],
-      appBar: AppBar(
-        backgroundColor: Colors.blue[900],
-        title: Text('Choose Location'),
-        centerTitle: true,
-        elevation: 0,
-      ),
-      // ignore: deprecated_member_use
-      body: RaisedButton(
-        onPressed: () {
-          setState(() {
-            counter += 1;
-          });
-        },
-        child: Text('counter is $counter'),
-      ),
-    );
+        backgroundColor: Colors.grey[200],
+        appBar: AppBar(
+          backgroundColor: Colors.blue[900],
+          title: Text('Choose Location'),
+          centerTitle: true,
+          elevation: 0,
+        ),
+        // ignore: deprecated_member_use
+        body: Text(''));
   }
 }
