6e1b19990479ef7339832da0799eaa66867f6c5d
Mahendra022 <ekamahendra86@gmail.com>
2021-06-16 19:58:13 +0700

    Learn15_Project2-Widget lifecycles

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -6,10 +6,34 @@ class ChooseLocation extends StatefulWidget {
 }
 
 class _ChooseLocationState extends State<ChooseLocation> {
+  int counter = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    print('initState function ran');
+  }
+
   @override
   Widget build(BuildContext context) {
+    print('build function ran');
     return Scaffold(
-      body: Text('choose location screen'),
+      backgroundColor: Colors.grey[200],
+      appBar: AppBar(
+        backgroundColor: Colors.blue[900],
+        title: Text('Choose Location'),
+        centerTitle: true,
+        elevation: 0,
+      ),
+      // ignore: deprecated_member_use
+      body: RaisedButton(
+        onPressed: () {
+          setState(() {
+            counter += 1;
+          });
+        },
+        child: Text('counter is $counter'),
+      ),
     );
   }
 }
